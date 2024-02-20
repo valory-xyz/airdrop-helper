a73e12950d46fd6c48aab75cf50ffe339d2a948e
David Vilela <dvilelaf@gmail.com>
2024-02-20 15:40:01 +0100

    fix: docs

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# airdrop-helper
+# Airdrop-helper
+
 A tool to measure participation in the Olas ecosystem
+
+
+## How to use
+
+1. Clone this repo
+
+2. Install [Python 3.10](https://www.python.org/downloads/) and [Poetry](https://python-poetry.org/docs/)
+
+3. Prepare the virtual environment and install dependencies
+    ```bash
+    cd packages
+    poetry shell
+    poetry install
+    ```
+
+4. Explore the examples in the script at [packages/example.py](https://github.com/valory-xyz/airdrop-helper/blob/main/packages/example.py), for example:
+
+    [!NOTE] Some of the following tools require API keys or RPCs to be set in a .env file. Read the example script for more information.
+
+    ```python
+    # Instantiate the Olas airdrop helper
+    olas = Olas()
+
+    # Get all Contribute users that have at least 50k points
+    contributors = olas.contributors.get(min_points=50000)
+
+    # Get veOLAS holders at a given block
+    veolas_holders = olas.veolas_holders.get(block=19263301)
+    ``````
--- packages/example.py
+++ packages/example.py
@@ -17,37 +17,50 @@
 #
 # ------------------------------------------------------------------------------
 
-"""Tests and examples"""
+"""Examples"""
 
 from olas import Olas
 import json
 
+#############################################################################
+# WARNING: some of the following tools require API keys or RPCs to be set   #
+# in a .env file. Please rename the sample.env file to .env and fill in the #
+# RPCs/keys you need to retrieve the required data.                         #
+#############################################################################
+
+# Instantiate the Olas airdrop helper
 olas = Olas()
 
-# Contribute
+# Get all Contribute users that have at least 50k points
 contributors = olas.contributors.get(min_points=50000)
 print("\nContributors\n", json.dumps(contributors, indent=4))
 
-# Snapshot votes
+# Get addresses that have voted at least 3 times on Snapshot
 snapshot_voters = olas.voters.snapshot.get(min_votes=3)
 print("\nSnapshot voters\n", json.dumps(snapshot_voters, indent=4))
 
-# Boardroom votes - requires an API key to be set in a .env file
+# Get addresses that have voted at least 3 times on Boardroom
+# Requires BOARDROOM_API_KEY to be set in the .env file
+# See https://docs.boardroom.io/docs/api/cd5e0c8aa2bc1-overview#request-an-api-key
 boardroom_voters = olas.voters.boardroom.get()
 print("\nBoardroom voters\n", json.dumps(boardroom_voters, indent=4))
 
-# veOLAS holders - requires an RPC to be set in a .env file
+# Get veOLAS holders at a given block (ignore block argument to retrieve latest)
+# Requires ETHEREUM_RPC to be set in the .env file
 veolas_holders = olas.veolas_holders.get(block=19263301)
 print("\nveOLAS holders\n", json.dumps(veolas_holders, indent=4))
 
-# NFT owners - requires an RPC to be set in a .env file
+# Get service, agent and component NFT owners
+# Requires RPC to be set in the .env file for all the chains you need to interact with
 nft_holders = olas.nft_owners.get()
 print("\nNFT owners\n", json.dumps(nft_holders, indent=4))
 
-# Stakers - requires an RPC to be set in a .env file
+# Get stakers who have participated in Everest or Alpine programmes
+# Requires GNOSIS_RPC to be set in the .env file
 stakers = olas.stakers.get()
 print("\nStakers\n", json.dumps(stakers, indent=4))
 
-# Bonders - requires an RPC to be set in a .env file
-bonders = olas.bonders.get()
+# Get addresses that have bonded up to a given block (ignore block argument to retrieve latest)
+# Requires ETHEREUM_RPC to be set in the .env file
+bonders = olas.bonders.get(block=19269490)
 print("\nBonders\n", json.dumps(bonders, indent=4))
